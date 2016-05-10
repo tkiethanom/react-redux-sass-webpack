905362a9e2a605f2a0a4436319c225dedfab73d7
Tawin Kiethanom <tawin.k@gmail.com>
2016-05-10 10:28:44 -0700

    Added url-loader and resolve url.

--- webpack.config.js
+++ webpack.config.js
@@ -20,15 +20,15 @@ module.exports = {
   devtool: (IS_DEV) ? "source-map" : '',
   module: {
     loaders: [
-      { test: /\.css$/, loaders: ["style", "css"] },
+      { test: /\.css$/, loaders: ["style", "css", "resolve-url"] },
       { test: /\.scss$/,
         loaders: (IS_DEV)
-          ? ["style", "css?sourceMap", "sass?sourceMap"]
-          : ["style", "css", "sass"]
+          ? ["style", "css", "resolve-url", "sass?sourceMap"]
+          : ["style", "css", "resolve-url", "sass"]
       },
 
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000" },
 
       { test: /\.(js|jsx)$/,
         exclude: /(node_modules)/,
